193392ff9b4275e3f4a7886237e5178af9f184bc
amysaad <150072514+amysaad@users.noreply.github.com>
2024-05-07 00:55:17 -0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,12 @@
+// functions.js
+
+// Function to display a message
+function showMessage() {
+    alert("Hello! Welcome to my portfolio website.");
+}
+
+// Function to toggle a CSS class
+function toggleClass() {
+    var element = document.getElementById("example");
+    element.classList.toggle("highlight");
+}
